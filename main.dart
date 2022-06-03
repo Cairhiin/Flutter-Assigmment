e524176fa2ed498d72841f0d7ae55276ba2eb626
Frank van de Voorde <daemonaz@kymp.net>
2022-06-03 21:29:24 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CalculatorAssignment());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class CalculatorAssignment extends StatelessWidget {
+  const CalculatorAssignment({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
