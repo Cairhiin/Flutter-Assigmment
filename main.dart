3e1f067416b7fb871b19bba64c8fab0f132871d3
Frank van de Voorde <daemonaz@kymp.net>
2022-06-08 19:46:54 +0300

    Update main.dart
    Style changes & Code refactoring

--- main.dart
+++ main.dart
@@ -9,15 +9,21 @@ class CalculatorAssignment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData(
+        brightness: Brightness.light,
+        primaryColor: const Color(0xffFF9800),
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0xffFF9800)));
     return MaterialApp(
-      title: 'Calculator',
+      title: 'Calculator Assignment',
+      theme: theme.copyWith(
+        colorScheme:
+            theme.colorScheme.copyWith(secondary: const Color(0xffFFE0B2)),
+      ),
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 198, 172, 143),
         appBar: AppBar(
-            backgroundColor: const Color.fromARGB(255, 127, 50, 70),
             title: const Center(
-              child: Text('Calculator'),
-            )),
+          child: Text('Calculator'),
+        )),
         body: const Center(
           child: Calculator(),
         ),
@@ -64,47 +70,56 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle circularStyle = ElevatedButton.styleFrom(
+      primary: Theme.of(context).colorScheme.secondary,
+      shape: const CircleBorder(),
+      minimumSize: const Size(200, 200),
+      elevation: 2,
+    );
+
+    final ButtonStyle normalStyle = ElevatedButton.styleFrom(
+      primary: Theme.of(context).primaryColor,
+      elevation: 4,
+      minimumSize: const Size(100, 30),
+    );
+
+    const TextStyle displayText = TextStyle(fontSize: 15, color: Colors.white);
+
+    String getResult(String a, String b, String operand) {
+      return operand == '+'
+          ? (int.parse(value1) + int.parse(value2)).toString()
+          : (int.parse(value1) - int.parse(value2)).toString();
+    }
+
     return Column(children: [
       Expanded(
         child: Container(
-          constraints: const BoxConstraints(minWidth: 400, maxWidth: 500),
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
+          constraints: const BoxConstraints(minWidth: 400.0, maxWidth: 500.0),
+          margin: const EdgeInsets.all(15),
+          padding: const EdgeInsets.only(right: 10.0, top: 10.0),
           color: const Color.fromARGB(255, 10, 9, 8),
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Expanded(
                     child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text(value1,
-                        style: const TextStyle(
-                          fontSize: 15,
-                          color: Color.fromARGB(255, 234, 224, 213),
-                        )),
+                    Text(value1, style: displayText),
                     const SizedBox(width: 20),
-                    Text(operandType,
-                        style: const TextStyle(
-                          fontSize: 15,
-                          color: Color.fromARGB(255, 234, 224, 213),
-                        )),
+                    Text(operandType, style: displayText),
                     const SizedBox(width: 20),
-                    Text(value2,
-                        style: const TextStyle(
-                          fontSize: 15,
-                          color: Color.fromARGB(255, 234, 224, 213),
-                        ))
+                    Text(value2, style: displayText)
                   ],
                 )),
                 Container(
-                  padding: const EdgeInsets.all(25),
-                  alignment: Alignment.centerRight,
+                  alignment: Alignment.bottomRight,
                   child: Text(
                     result,
                     style: const TextStyle(
                         fontSize: 38,
-                        color: Color.fromARGB(255, 234, 224, 213),
+                        color: Colors.white,
                         fontWeight: FontWeight.bold),
                   ),
                 )
@@ -114,6 +129,8 @@ class _CalculatorState extends State<Calculator> {
       Expanded(
           flex: 3,
           child: Container(
+              constraints:
+                  const BoxConstraints(minWidth: 400.0, maxWidth: 500.0),
               padding: const EdgeInsets.all(15),
               child: GridView.builder(
                   primary: false,
@@ -173,16 +190,8 @@ class _CalculatorState extends State<Calculator> {
                                         if (buttons[index] == '=' &&
                                             value1 != '' &&
                                             value2 != '') {
-                                          if (operandType == '+') {
-                                            // parse the values to int then add or substract them and convert them back to String
-                                            result = (int.parse(value1) +
-                                                    int.parse(value2))
-                                                .toString();
-                                          } else {
-                                            result = (int.parse(value1) -
-                                                    int.parse(value2))
-                                                .toString();
-                                          }
+                                          result = getResult(
+                                              value1, value2, operandType);
 
                                           // resetting values
                                           value1 = value2 = operandType = '';
@@ -192,26 +201,13 @@ class _CalculatorState extends State<Calculator> {
                               child: Text(
                                 buttons[index],
                                 style: const TextStyle(
-                                  fontSize: 25,
+                                  color: Colors.black,
+                                  fontSize: 28,
                                   fontWeight: FontWeight.w400,
-                                  color: Color.fromARGB(255, 234, 224, 213),
                                 ),
                               ),
                             )));
                   }))),
     ]);
   }
-
-  final ButtonStyle circularStyle = ElevatedButton.styleFrom(
-    primary: const Color.fromARGB(255, 94, 80, 63),
-    shape: const CircleBorder(),
-    minimumSize: const Size(200, 200),
-    elevation: 8,
-  );
-
-  final ButtonStyle normalStyle = ElevatedButton.styleFrom(
-    primary: const Color.fromARGB(255, 34, 51, 59),
-    elevation: 4,
-    minimumSize: const Size(100, 30),
-  );
 }
